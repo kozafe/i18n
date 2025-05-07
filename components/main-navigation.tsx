7bafc1c33b0d3963ee29966129482ef2f2ce22be
koza <k.brajamagenta@gmail.com>
2025-05-07 22:09:58 +0700

    successfully implemented translations

--- components/main-navigation.tsx
+++ components/main-navigation.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-import { usePathname } from "next/navigation"
-import { cn } from "@/lib/utils"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const navItems = [
   { name: "Home", href: "/" },
@@ -11,11 +11,12 @@ const navItems = [
   { name: "Tasks", href: "/tasks" },
   { name: "Reporting", href: "/reporting" },
   { name: "Users", href: "/users" },
-]
+];
 
 export default function MainNavigation() {
-  const pathname = usePathname()
-
+  const pathname = usePathname();
+  // const t = useTranslations("home");
+  // return t("test");
   return (
     <nav className="flex items-center space-x-4 lg:space-x-6">
       {navItems.map((item) => (
@@ -24,12 +25,12 @@ export default function MainNavigation() {
           href={item.href}
           className={cn(
             "text-sm font-medium transition-colors hover:text-primary",
-            pathname === item.href ? "text-primary" : "text-muted-foreground",
+            pathname === item.href ? "text-primary" : "text-muted-foreground"
           )}
         >
           {item.name}
         </Link>
       ))}
     </nav>
-  )
+  );
 }
